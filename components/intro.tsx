df55e2c2061cba78c905cc676000d48d1a4b2aa2
Gautam Sidhanth <gautam@altrodav.com>
2025-12-19 04:10:00 +0530

    sixth push

--- components/intro.tsx
+++ components/intro.tsx
@@ -94,7 +94,7 @@ export default function Intro() {
           </Link>
 
           <a
-            className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10 shadow-lg hover:shadow-xl"
+            className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60 shadow-lg hover:shadow-xl"
             href="/CV.pdf"
             download="Gautam_Sidhanth_CV.pdf"
           >
